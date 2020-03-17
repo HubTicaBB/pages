e390661a070d1829a04376d1cf8c7c7bea937be3
Tijana <tijana.ilic87@protonmail.com>
2020-03-17 01:26:24 +0100

    Add fetch response validation

--- bookstore/script.js
+++ bookstore/script.js
@@ -6,12 +6,19 @@ document.getElementById('request-api-key-button').addEventListener('click', fetc
 
 function fetchAPI() {
     fetch('https://www.forverkliga.se/JavaScript/api/crud.php?requestKey')
-        .then((response) => {
+    .then((response) => {
+        if (response.status !== 200) {
+            // TODO: alla AJAX-anrop som misslyckas upprepas tills de går igenom (men maximalt 10 gånger)
+            console.log('Fetching failed: status code: ' + response.status);
+            return;
+        }
+        else {
             return response.json();
-        })
-        .then((data) => {
-            APIKey = data.key;    
-            document.getElementById('api-key').textContent = APIKey;     
-            localStorage.setItem('item' + localStorage.length, APIKey);
-        })
+        }
+    })
+    .then((data) => {
+        APIKey = data.key;    
+        document.getElementById('api-key').textContent = APIKey;     
+        localStorage.setItem('item' + localStorage.length, APIKey);
+    })
 }
