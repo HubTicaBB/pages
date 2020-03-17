a1347e5e89dbaef955e3493b947110b2c4c08ae8
Tijana <tijana.ilic87@protonmail.com>
2020-03-17 22:58:20 +0100

    Create functions to add a new book

--- bookstore/script.js
+++ bookstore/script.js
@@ -1,20 +1,22 @@
-var APIKey, localStorage;
+var APIKey, localStorage, APIUrl;
 
 window.onload = initialize();
 
 
 function initialize() {
+    APIUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php';
     fetchAPI();
     localStorage = window.localStorage;
     localStorage.clear();
     document.getElementById('add-form').style.display = 'none';
     document.getElementById('request-api-key-button').addEventListener('click', fetchAPI);
     document.getElementById('add-button').addEventListener('click', toggleAddBookForm);
-    document.getElementById('submit-book-button').addEventListener('click', addBook);
+    document.getElementById('submit-book-button').addEventListener('click', function(e) {addBook();});    
+    document.getElementById('close').addEventListener('click', closeForm);
 }
 
 function fetchAPI() {
-    fetch('https://www.forverkliga.se/JavaScript/api/crud.php?requestKey')
+    fetch(APIUrl + '?requestKey')
     .then((response) => {
         if (response.status !== 200) {
             // TODO: alla AJAX-anrop som misslyckas upprepas tills de går igenom (men maximalt 10 gånger)
@@ -33,20 +35,56 @@ function fetchAPI() {
 function toggleAddBookForm() {
     var addBookForm = document.getElementById('add-form');
     addBookForm.style.display = (addBookForm.style.display === 'none') ? 'block' : 'none';
+    if (addBookForm.style.display === 'block') {
+        document.getElementById('input-title').addEventListener('input', function() {validateInput(this)});
+        document.getElementById('input-author').addEventListener('input', function() {validateInput(this)}); 
+    }
 }
 
 function validateInput(inputField) {
-    if (inputField.value.length > 0) { // TODO: Lägga till regex validation (alphanummeric)
-        console.log('inputField.textContent.length - ' + inputField.value.length);
-        inputField.style.outlineColor = 'green';
-    }
-    else {
-        console.log('inputField.textContent.length - ' + inputField.value.length);
-        inputField.style.outlineColor = 'red';
-    }
+    inputField.style.outlineColor = (inputField.value.length > 0) ? 'green' : 'red';
 }
 
-function addBook() {
+let counter = 1;
+function addBook(e) {    
+    let statusLabel =  document.getElementById('add-status');
+    statusLabel.textContent = 'Submitting request...';  
+    statusLabel.style.background = 'grey';
+
+    fetch(APIUrl + '?key=' + APIKey + '&op=insert&title=' + document.getElementById('input-title').value + '&author=' + document.getElementById('input-author').value)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        if (data.status !== 'success' && counter <= 10) {
+            counter++; 
+            addBook(e);
+        }
+        else {
+            updateStatus(data, e);
+            counter = 0;
+        }
+    })
 }
 
+function updateStatus(data, e) {
+    let statusLabel = document.getElementById('add-status');
+    statusLabel.textContent = 'Status:\t' + data.status;
+    if (data.status === 'success') {
+        statusLabel.style.background = '#97c98b';
+        updateBookView(data, e);
+    } 
+    else  if (data.status === 'error') {
+        statusLabel.textContent += ' - ' + data.message;
+        statusLabel.style.background = 'pink';
+    } 
+}
 
+function updateBookView(data, e) {
+    var bookView = document.getElementById('book-list');
+    bookView.innerHTML += '<tr><td class="id">' + data.id + '</td><td class="author">' + document.getElementById('input-author').value + '</td><td class="title">' + document.getElementById('input-title').value + '</td><td class="actions"><i class="fa fa-edit fa-2x"></i><i class="fa fa-trash fa-2x"></i></td></tr>';
+}
+
+function closeForm() {
+    document.getElementById('add-form').style.display = 'none';
+}
