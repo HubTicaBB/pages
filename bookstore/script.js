5e6ad540ed9d7bf29e443738a11e8d635b910307
Tijana <tijana.ilic87@protonmail.com>
2020-03-17 01:12:52 +0100

    Create 'fetchAPI' function, add click event to 'Request API key' button to call the fuction

--- bookstore/script.js
+++ bookstore/script.js
@@ -1,1 +1,13 @@
-console.log('Just testing');
+var APIKey;
+
+document.getElementById('request-api-key-button').addEventListener('click', fetchAPI());
+
+function fetchAPI () {
+    fetch('https://www.forverkliga.se/JavaScript/api/crud.php?requestKey')
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            APIKey = data.key;            
+        })
+}
