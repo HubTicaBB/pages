a172fb54a7d164ef21742da491db05e2bb506b86
Tijana <tijana.ilic87@protonmail.com>
2020-03-18 01:52:48 +0100

    Add editBook func

--- bookstore/script.js
+++ bookstore/script.js
@@ -8,9 +8,9 @@ function initialize() {
     fetchAPI();
     localStorage = window.localStorage;
     localStorage.clear();
-    document.getElementById('add-form').style.display = 'none';
+    document.getElementById('form').style.display = 'none';
     document.getElementById('request-api-key-button').addEventListener('click', fetchAPI);
-    document.getElementById('add-button').addEventListener('click', toggleAddBookForm);
+    document.getElementById('add-button').addEventListener('click', toggleForm);
     document.getElementById('submit-book-button').addEventListener('click', addBook);    
     document.getElementById('close').addEventListener('click', function() {closeForm(this.parentNode.parentNode)});
 }
@@ -32,10 +32,11 @@ function fetchAPI() {
     })
 }
 
-function toggleAddBookForm() {
-    var addBookForm = document.getElementById('add-form');
-    addBookForm.style.display = (addBookForm.style.display === 'none') ? 'block' : 'none';
-    if (addBookForm.style.display === 'block') {
+function toggleForm() {
+    var form = document.getElementById('form');
+    form.style.display = (form.style.display === 'none') ? 'block' : 'none';
+    if (form.style.display === 'block') {
+        document.getElementById('heading').innerHTML = "Add Book Details";
         document.getElementById('input-title').addEventListener('input', function() {validateInput(this)});
         document.getElementById('input-author').addEventListener('input', function() {validateInput(this)}); 
     }
@@ -82,12 +83,10 @@ function updateStatus(data) {
 
 function updateBookView(data) {
     var bookView = document.getElementById('book-list');
-    bookView.innerHTML += '<tr id="' + data.id + '"><td class="id">' + data.id + '</td><td class="author">' + document.getElementById('input-author').value + '</td><td class="title">' + document.getElementById('input-title').value + '</td><td class="actions"><i class="fa fa-edit fa-2x onclick="editBook(' + data.id + '"></i><i class="fa fa-trash fa-2x" onclick="deleteBook(' + data.id + ')"></i></td></tr>';
+    bookView.innerHTML += '<tr id="' + data.id + '"><td class="id">' + data.id + '</td><td class="author">' + document.getElementById('input-author').value + '</td><td class="title">' + document.getElementById('input-title').value + '</td><td class="actions"><i class="fa fa-edit fa-2x" onclick="editBook(' + data.id + ')"></i><i class="fa fa-trash fa-2x" onclick="deleteBook(' + data.id + ')"></i></td></tr>';
 }
 
 function closeForm(form) {
-    console.log(form);
-    console.log(form.id);
     document.getElementById(form.id).style.display = 'none';
 }
 
@@ -112,5 +111,6 @@ function deleteBook(id) {
 }
 
 function editBook(id) {
-
+    toggleForm();
+    document.getElementById('heading').innerHTML = "Modify Book Details";
 }
